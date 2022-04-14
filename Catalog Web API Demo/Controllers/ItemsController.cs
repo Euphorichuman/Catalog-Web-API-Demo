3ddd2508f77d840199f7e52998fe2656f3379abe
Martin Fuentes <mefuentes@uninorte.edu.co>
2022-04-14 15:12:20 -0500

    Refactoring indentation

--- Catalog Web API Demo/Controllers/ItemsController.cs
+++ Catalog Web API Demo/Controllers/ItemsController.cs
@@ -5,36 +5,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog_Web_API_Demo.Controllers;
     
-    [ApiController]
-    [Route("items")]
-    public class ItemsController : ControllerBase
-    {
-        private readonly IItemsRepository repository;
+[ApiController]
+[Route("items")]
+public class ItemsController : ControllerBase
+{
+    private readonly IItemsRepository repository;
 
-        public ItemsController(IItemsRepository repository)
-        {
-            this.repository = repository;
-        }
-        
-        // GET /items
-        [HttpGet]
-        public IEnumerable<ItemDTO> GetItems()
-        {
-            var items = repository.GetItems().Select(item => item.AsDTO());
-            return items;
-        }
-        
-        // Get /items/{id}
-        [HttpGet("{id}")]
-        public ActionResult<ItemDTO> GetItem(Guid id)
-        {
-            var item = repository.GetItem(id);
-            
-            if (item == null)
-            {
-                return NotFound();
-            }
-            
-            return item.AsDTO();
-        }    
+    public ItemsController(IItemsRepository repository)
+    {
+        this.repository = repository;
     }
+        
+    // GET /items
+    [HttpGet]
+    public IEnumerable<ItemDTO> GetItems()
+    {
+        var items = repository.GetItems().Select(item => item.AsDTO());
+        return items;
+    }
+        
+    // Get /items/{id}
+    [HttpGet("{id}")]
+    public ActionResult<ItemDTO> GetItem(Guid id)
+    {
+        var item = repository.GetItem(id);
+            
+        if (item == null)
+        {
+            return NotFound();
+        }
+            
+        return item.AsDTO();
+    }
+    
+        
+}
--- Catalog Web API Demo/Repositories/IItemsRepository.cs
+++ Catalog Web API Demo/Repositories/IItemsRepository.cs
@@ -1,8 +1,8 @@
 ﻿using Catalog_Web_API_Demo.Entities;
 
 namespace Catalog_Web_API_Demo.Repositories;
-    public interface IItemsRepository
-    {
-        Item GetItem(Guid id);
-        IEnumerable<Item> GetItems();
-    }
+public interface IItemsRepository
+{
+    Item GetItem(Guid id);
+    IEnumerable<Item> GetItems();
+}
