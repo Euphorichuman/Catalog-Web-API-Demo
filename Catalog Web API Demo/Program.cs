700cef0218906073dce1088903013c49c3021f1f
Martin Fuentes <mefuentes@uninorte.edu.co>
2022-04-15 00:18:26 -0500

    Add Item Entity persistence with MongoDB

--- Catalog Web API Demo/Program.cs
+++ Catalog Web API Demo/Program.cs
@@ -1,11 +1,26 @@
 using Catalog_Web_API_Demo.Repositories;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Serialize the representation of Guid and DateTimeOff mongoDN database 
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+// Perform registration of IMongoClient
+builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
+{
+    var settings = builder.Configuration.GetConnectionString("MongoDB");
+    return new MongoClient(settings);
+});
+
 // Singleton for having one copy of the instance of a type across the entire life of the service
-builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+builder.Services.AddSingleton<IItemsRepository, MongoDBItemsRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Catalog Web API Demo/Settings/MongoDBSettings.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+
+namespace Catalog_Web_API_Demo.Settings;
+
+public class MongoDBSettings
+{
+    public string Host { get; set; }
+
+    public int Port { get; set; }
+
+    public string ConnectionString { get
+        {
+            return $"mongodb://{Host}:{Port}";
+        } 
+    }
+}
